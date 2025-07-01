a755b8c646f48f1da50fa3a04f95ba11217a084d
Kyle Chisholm <kyle.chisholm2@gmail.com>
2025-07-01 09:50:49 +0800

    Add fish house

--- src/main.js
+++ src/main.js
@@ -2,16 +2,25 @@ import './style.css'
 import '@google/model-viewer'
 import { generateGlbCard } from './glb_card.js'
 
-const params = {
+const jay_params = {
   source: "assets/green-coral.glb",
   description: "Green coral captured from underwater camera at DinaCon",
   title: "Green Coral",
   author: "Jay"
 };
-const jay_glb = generateGlbCard(params);
+const jay_glb = generateGlbCard(jay_params);
+
+const fish_house_params = {
+  source: "assets/fish-house.glb",
+  description: "Mound for coral restoration and a home for fishies",
+  title: "Fish House",
+  author: "Grace"
+};
+const fish_house_glb = generateGlbCard(fish_house_params);
 
 document.querySelector('#app').innerHTML = `
   <div class="card-container">
     ${jay_glb}
+    ${fish_house_glb}
   </div>
 `
